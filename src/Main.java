f8bfa3cde70793d02d74b3f951b9aa098e8d0a5a
Ivanesscob <ivanesscob@gmail.com>
2024-03-01 11:33:04 +0300

    first Graph in java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+            ConntactForm form = new ConntactForm();
+           form.setVisible(true);
+        StringBuilder sb = new StringBuilder("g");
+        System.out.println(sb);
+        }
+    }
